d4041b51f1e01d349fd077aef804a9a196b169a4
Ram <rameswareddy78@gmail.com>
2025-08-12 23:13:30 +0530

    origin

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://form-frentends.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://form-frentends.vercel.app","https://form-frentends.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
